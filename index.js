be42108c022ac0d085dbb4c85c34b759b22269e2
Maria Morando <mnoel.morando@gmail.com>
2023-11-22 18:41:56 +0100

    configurando rutas

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const http = require('http')
-const app = require('.')
+const app = require('./src/app')
+require('dotenv')
+require('./src/config/db')
+
+const server = http.createServer(app)
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT)
+server.on('listening', () => {
+    console.log(`Servidor escuchando en puerto ${PORT}`)
+})
+server.on('error', (error) => {
+    console.log(error)
+})
---
+++ src/routes/api.js
@@ -0,0 +1,4 @@
+const router = require('express').Router()
+router.use('/usuarios', require('./api/usuarios'))
+
+module.exports = router
