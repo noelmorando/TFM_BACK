a5fbc9f7ef9a68e27d0c4d1472f7184e74b34f53
Maria Morando <mnoel.morando@gmail.com>
2023-12-08 23:22:25 +0100

    .

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-const http = require('node:http')
-const app = require('./src/app')
-require('dotenv').config()
-require('./src/config/db')
+const http = require('node:http');
+const fs = require('fs');
+const app = require('./src/app');
+require('dotenv').config();
+require('./src/config/db');
 
-const server = http.createServer(app)
-const PORT = process.env.PORT || 3000
+const server = http.createServer(app);
+const PORT = process.env.PORT || 3000;
+const logFilePath = './erroreslog';
 
-server.listen(PORT)
-//TODO: Cambiar los console.log por lineas que se agregan en un fichero log.
-server.on('listening',()=>{
-    console.log(`Servidor escuchando en puerto ${PORT}`)
-})
-server.on('error', (error) => console.log(error))
+server.listen(PORT);
+
+server.on('listening', () => {
+    const successMessage = `Servidor escuchando en puerto ${PORT}\n`;
+    fs.appendFile('errores.log', successMessage, (err) => {
+        if (err) {
+            console.error('Error al escribir en el archivo de errores.log:', err);
+        }
+    });
+});
+
+server.on('error', (error) => {
+    // Agregar el error al archivo errores.log
+    const errorMessage = `Error en el servidor: ${error.message}\n${error.stack}\n\n`;
+    fs.appendFile(logFilePath, errorMessage, (err) => {
+        if (err) {
+            console.error('Error al escribir en el archivo de errores.log:', err);
+        }
+    });
+});
