429e57d9026e4f1c2af088d174f78cc03aada262
Maria Morando <mnoel.morando@gmail.com>
2023-12-08 23:41:57 +0100

    .

--- src/routes/api.js
+++ src/routes/api.js
@@ -3,4 +3,4 @@ const router = require('express').Router()
 router.use('/usuarios', require('./api/usuarios'))
 router.use('/especialidades', require('./api/especialidades'))
 
-module.exports = router; 
+module.exports = router;
